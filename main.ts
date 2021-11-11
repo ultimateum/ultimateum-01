ae5e853c4fc963d9b3173e4661e649af94b3b59b
ultimateum <94100065+ultimateum@users.noreply.github.com>
2021-11-11 16:13:39 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.StickFigure)
+    soundExpression.slide.playUntilDone()
+})
+input.onGesture(Gesture.Shake, function () {
+    music.startMelody(music.builtInMelody(Melodies.Wawawawaa), MelodyOptions.Once)
+})
+input.onButtonPressed(Button.AB, function () {
+    soundExpression.twinkle.playUntilDone()
+})
+input.onButtonPressed(Button.B, function () {
+    music.startMelody(music.builtInMelody(Melodies.Ode), MelodyOptions.Once)
+    led.plot(2, 3)
+})
 basic.forever(function () {
 	
 })
